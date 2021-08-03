41789236fb40503fa3ec0bbe8da5b7801225bde4
Alex Constantinou <a_constantinou@hotmail.co.uk>
2021-08-03 23:33:16 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main class="wrapper">
+        <div className="title">
+          <h1>Damnation</h1>
+          <img src="https://thegraphicsfairy.com/wp-content/uploads/2010/09/Vintage-Skull-Clip-Art-GraphicsFairy.jpg" alt="Several hands holding beer glasses"/>
+        </div>
+        <section class="articles">
+          <ul>
+            <li>
+              <figure>
+                <img src="https://static01.nyt.com/images/2020/10/30/arts/29rewind/merlin_179081037_a8f77a49-61db-4658-a5f7-a10f0b01d3b4-superJumbo.jpg?quality=90&auto=webp" alt="Several hands holding beer glasses"/>
+              </figure>
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </li>
+          </ul>
+        </section>
+        <section class="articles" id="articles">
+        <ul>
+          <li>
+            <figure>
+              <img src="https://static01.nyt.com/images/2020/10/30/arts/29rewind/merlin_179081037_a8f77a49-61db-4658-a5f7-a10f0b01d3b4-superJumbo.jpg?quality=90&auto=webp" alt="Several hands holding beer glasses"/>
+            </figure>
+            <h3>Lorem ipsum dolor sit amet</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </li>
+          <li>
+            <figure>
+              <img src="https://static01.nyt.com/images/2020/10/30/arts/29rewind/merlin_179081037_a8f77a49-61db-4658-a5f7-a10f0b01d3b4-superJumbo.jpg?quality=90&auto=webp" alt="Several hands holding beer glasses"/>
+            </figure>
+            <h3>Lorem ipsum dolor sit amet</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </li>
+          <li>
+            <figure>
+              <img src="https://static01.nyt.com/images/2020/10/30/arts/29rewind/merlin_179081037_a8f77a49-61db-4658-a5f7-a10f0b01d3b4-superJumbo.jpg?quality=90&auto=webp" alt="Several hands holding beer glasses"/>
+            </figure>
+            <h3>Lorem ipsum dolor sit amet</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </li>
+      </ul>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2021 Damnation</p>
+      </footer>
+    </>
   );
 }
 
