7d96beb5bb0ef8f83ca998b4cd8e87d363a54f09
Alex Constantinou <a_constantinou@hotmail.co.uk>
2021-09-01 14:51:28 +0100

    cursor displays on article summary divs

--- src/history.js
+++ src/history.js
@@ -1,3 +1,13 @@
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from 'history'
 
-export default createBrowserHistory();
+const history = createBrowserHistory()
+
+let prevLocation = {}
+history.listen((location) => {
+  const pathChanged = prevLocation.pathname !== location.pathname
+  const hashChanged = prevLocation.hash !== location.hash
+  if (pathChanged || hashChanged) window.scrollTo(0, 0)
+  prevLocation = location
+})
+
+export default history
